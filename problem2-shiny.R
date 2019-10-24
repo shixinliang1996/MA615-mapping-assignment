1cf67d6d967c0fc4c5e726901c650c2d23f14b21
sxliang <sxliang@bu.edu>
2019-10-23 23:42:04 -0400

    problem 2 shiny

--- problem2-shiny.R
+++ problem2-shiny.R
@@ -3,46 +3,58 @@ library(leaflet)
 library(tidyverse)
 library(leaflet.extras)
 
-#data <- read.csv( "earthquakes_1.0_month.csv", header = TRUE )
 data <- read.csv("Colleges and universities in Boston.csv", header = TRUE)
 
 #categorize university's student number
 data$NumStudent[data$NumStudent == 0] <- NA
-data$NumStudent[data$NumStudent <= 1000] <- "small"
-data$NumStudent[data$NumStudent < 5000] <- "medium"
-data$NumStudent[data$NumStudent == 9148] <- "large"
-data$NumStudent[data$NumStudent == 5003] <- "large"
+data$NumStudent[data$NumStudent <= 1000] <- "small (<1000)"
+data$NumStudent[data$NumStudent < 5000] <- "medium (1001~5000)"
+data$NumStudent[data$NumStudent == 9148] <- "large (>5000)"
+data$NumStudent[data$NumStudent == 5003] <- "large (>5000)"
 
+#categorize university's built year
 data$YearBuilt[data$YearBuilt == 0] <- NA
 data$YearBuilt[data$YearBuilt < 1900] <- "1800s"
 data$YearBuilt[data$YearBuilt >= 1900] <- "1900s"
 
+
 ui <- fluidPage(
-  mainPanel( 
-    #this will create a space to display our map
-    leafletOutput(outputId = "mymap"), 
-    textOutput("information"),
-    #put the checkmarks ontop of the map
-    absolutePanel(top = 80, left = 20, 
-                  checkboxInput("markers", "Number of Student", FALSE),
-                  checkboxInput("year", "Year Built", FALSE)
-    )
-  ))
+  
+  titlePanel("Exploration on Colleges and Universities in Boston"),
+  
+  navlistPanel(
+    tabPanel("Location of Colleges and Universities", 
+             leafletOutput(outputId = "mymap1"),
+             textOutput("information1")
+             ),
+    
+    tabPanel("Number of Student & Year Built", 
+             #this will create a space to display our map
+             leafletOutput(outputId = "mymap"), 
+             textOutput("information"),
+             #put the checkmarks ontop of the map
+             absolutePanel(top = 80, left = 20, 
+                           checkboxInput("markers", "Number of Student", FALSE),
+                           checkboxInput("year", "Year Built", FALSE)))
+
+  )
+)
+
 
 server <- function(input, output, session) {
-  #define the color pallate for the magnitidue of the earthquake
+  #define the color pallate for year built
   pal1 <- colorFactor(
     palette = c('blue', 'pink'),
     domain = data$YearBuilt)
    
-  #define the color of for the depth of the earquakes
+  #define the color of for number of student 
   pal2 <- colorFactor(
     palette = c('red', 'orange', 'yellow'),
     domain = data$NumStudent
   )
   
   #create the map
-  output$mymap <- renderLeaflet({
+  output$mymap1 <- renderLeaflet({
     leaflet(data = data) %>%
       setView(-71.10000, 42.32000, zoom = 11) %>% 
       addProviderTiles("CartoDB.Positron", group = "Map") %>%
@@ -57,11 +69,30 @@ server <- function(input, output, session) {
       )
   })
   
-  output$information <- renderText("We can see that most universities locate near Charles River.  
-                                   Schools that built in 1800s are more likely to be near Charles River while schools that built in 1900s are a little bit far from the center of Boston.  
-                                   And schools with large number of student locate in downtown area: near Boston and Fenway.")
   
-  #next we use the observe function to make the checkboxes dynamic. If you leave this part out you will see that the checkboxes, when clicked on the first time, display our filters...But if you then uncheck them they stay on. So we need to tell the server to update the map when the checkboxes are unchecked.
+  output$mymap <- renderLeaflet({
+    leaflet(data = data) %>%
+      setView(-71.10000, 42.32000, zoom = 11) %>% 
+      addProviderTiles("CartoDB.Positron", group = "Map") %>%
+      addProviderTiles("Esri.WorldImagery", group = "Satellite")%>% 
+      addProviderTiles("Esri.WorldShadedRelief", group = "Relief")%>%
+      addMarkers(~Longitude, ~Latitude, label = ~Name, group = "Universities") %>% 
+      addScaleBar(position = "bottomleft") %>%
+      addLayersControl(
+        baseGroups = c("Map", "Satellite", "Relief"),
+        overlayGroups = c("Universities"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+
+  output$information1 <- renderText("There are 60 colleges and universities in Boston.")
+  
+  
+  output$information <- renderText("We can see that most universities locate near Charles River.  
+                                   Universities that built in 1800s are more likely to be near Charles River while universities that built in 1900s are a little bit far from the center of Boston.  
+                                   And universities with large number of student locate in downtown area: near Boston and Fenway.")
+  
+  #next we use the observe function to make the checkboxes dynamic. 
   observe({
     proxy <- leafletProxy("mymap", data = data)
     proxy %>% clearMarkers()
