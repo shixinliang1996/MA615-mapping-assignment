7b21ad4db034aeee1bc772e9dc26c41de1a26d77
sxliang <sxliang@bu.edu>
2019-10-23 21:06:10 -0400

    problem 2 update

--- problem2.R
+++ problem2.R
@@ -4,13 +4,6 @@ library(htmlwidgets) # To save the map as a web page.
 
 sites <- read.csv("Colleges and universities in Boston.csv", header = TRUE)
 bounds <- map('state', c('Massachusetts'), fill=TRUE, plot=FALSE)
-icons <- awesomeIcons(
-  icon = 'disc',
-  iconColor = 'black',
-  library = 'ion', # Options are 'glyphicon', 'fa', 'ion'.
-  markerColor = 'blue',
-  squareMarker = TRUE
-)
 
 map <- leaflet(data = sites) %>%
   setView(-71.10000, 42.32000, zoom = 11) %>% 
@@ -18,7 +11,6 @@ map <- leaflet(data = sites) %>%
   addProviderTiles("Esri.WorldImagery", group = "Satellite")%>% 
   addProviderTiles("Esri.WorldShadedRelief", group = "Relief")%>%
   addMarkers(~Longitude, ~Latitude, label = ~Name, group = "Universities") %>% 
-  # addAwesomeMarkers(~lon_dd, ~lat_dd, label = ~locality, group = "Sites", icon=icons) %>%
   addPolygons(data=bounds, group="States", weight=2, fillOpacity = 0) %>%
   addScaleBar(position = "bottomleft") %>%
   addLayersControl(
